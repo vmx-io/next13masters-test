15b7a80e796fab3270944238ac313cdb238070fb
Damian Kubiczek <d.kubiczek@reply.com>
2024-05-30 13:44:57 +0200

    Vercel test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             By{" "}
             <Image
               src="/vercel.svg"
-              alt="Vercel Logo"
+              alt="Vercel Logo test"
               className="dark:invert"
               width={100}
               height={24}
